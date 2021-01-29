e2265e87c721e03f350cf22eb8406114cc3a4bb8
Awadhila <alrae.awadh@gmail.com>
2021-01-29 17:33:11 +0300

    Sprint 1 | 1st commit •	Install xampp for local hosting. •	Install composer. •	Install node.js. •	Setup Laravel project ARIS Akuyaz retail Information system •	Setup Bootstrap and Jquery •	Setup Laravel library fortify for authentication. •	Setup poper.js for pop-ups •	Setup Laravel fortify

--- routes/web.php
+++ routes/web.php
@@ -11,7 +11,21 @@ use Illuminate\Support\Facades\Route;
 | routes are loaded by the RouteServiceProvider within a group which
 | contains the "web" middleware group. Now create something great!
 |
-*/
-Route::get('/', function () {
-    return view('welcome');
+/*
+Route::get('/hello', function () {
+    //return view('welcome');
+    return '<h1>Hello World</h1>';
 });
+Route::get('/users/{id}/{name}', function($id, $name){
+    return 'This is user '.$name.' with an id of '.$id;
+});
+*/
+
+Route::get('/', function () {
+    return view('auth.login');
+});
+
+Route::get('/home', function () {
+    return view('home');
+})->middleware('auth');
+
