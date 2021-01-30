410ad44545f87d8d490d38e21beada14e79cc9f6
Awadhila <alrae.awadh@gmail.com>
2021-01-30 17:31:30 +0300

    Sprint 1 commit 2 Setup Controllers for views **dashboard **Login **Register

--- routes/web.php
+++ routes/web.php
@@ -1,31 +1,9 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Auth\loginController;
+use App\Http\Controllers\Auth\registerController;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-/*
-Route::get('/hello', function () {
-    //return view('welcome');
-    return '<h1>Hello World</h1>';
-});
-Route::get('/users/{id}/{name}', function($id, $name){
-    return 'This is user '.$name.' with an id of '.$id;
-});
-*/
 
-Route::get('/', function () {
-    return view('auth.login');
-});
-
-Route::get('/home', function () {
-    return view('home');
-})->middleware('auth');
-
+Route::get('/',[registerController::class, 'index']);
+Route::get('/home',[loginController::class, 'index'])->middleware('auth');
